04de0b2c6c0b408609c09a9307f0b174d065e1a2
jfrumkin-ucla <159074131+jfrumkin-ucla@users.noreply.github.com>
2025-02-07 12:18:14 -0600

    commit

---
+++ test/configs/backstop_data/html_report/config.js
@@ -0,0 +1,60 @@
+report({
+  "testSuite": "BackstopJS",
+  "tests": [
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_BackstopJS_Homepage_0_document_0_phone.png",
+        "test": "../bitmaps_test/20250207-115210/backstop_default_BackstopJS_Homepage_0_document_0_phone.png",
+        "selector": "document",
+        "fileName": "backstop_default_BackstopJS_Homepage_0_document_0_phone.png",
+        "label": "BackstopJS Homepage",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "https://apo.ucla.edu/",
+        "referenceUrl": "https://dev-ucla-siteden-apo.pantheonsite.io/",
+        "expect": 0,
+        "viewportLabel": "phone",
+        "diff": {
+          "isSameDimensions": false,
+          "dimensionDifference": {
+            "width": 0,
+            "height": -34
+          },
+          "rawMisMatchPercentage": 29.593553848695077,
+          "misMatchPercentage": "29.59",
+          "analysisTime": 45
+        },
+        "diffImage": "../bitmaps_test/20250207-115210/failed_diff_backstop_default_BackstopJS_Homepage_0_document_0_phone.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_BackstopJS_Homepage_0_document_1_tablet.png",
+        "test": "../bitmaps_test/20250207-115210/backstop_default_BackstopJS_Homepage_0_document_1_tablet.png",
+        "selector": "document",
+        "fileName": "backstop_default_BackstopJS_Homepage_0_document_1_tablet.png",
+        "label": "BackstopJS Homepage",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "https://apo.ucla.edu/",
+        "referenceUrl": "https://dev-ucla-siteden-apo.pantheonsite.io/",
+        "expect": 0,
+        "viewportLabel": "tablet",
+        "diff": {
+          "isSameDimensions": false,
+          "dimensionDifference": {
+            "width": 0,
+            "height": -72
+          },
+          "rawMisMatchPercentage": 30.986149203974588,
+          "misMatchPercentage": "30.99",
+          "analysisTime": 80
+        },
+        "diffImage": "../bitmaps_test/20250207-115210/failed_diff_backstop_default_BackstopJS_Homepage_0_document_1_tablet.png"
+      },
+      "status": "fail"
+    }
+  ],
+  "id": "backstop_default"
+});
